af0f08edbcc66226e64bdbb34eb1e07489056aa6
HDallol <marcodallopolveni@gmail.com>
2023-04-09 01:08:01 +0200

    - Aggiunte sala archeologia e attrezzi. - Aggiunta pagina didattica. - Aggiunto fonte su pagina ambiente (parco colli euganei) - Aggiunto un handler per l'errore 404

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ def getImgs(path, name):
 
 app = Flask(__name__)            
 
-nsale = 3       # Numero di sale
+NSALE = 5       # Numero di sale
 
 sala1 = {
         "nomeSala": "Sala A",
@@ -77,6 +77,31 @@ sala3 = {
         "imgMainSala": "static/imgs/salaC/mainSalaC.jpg"
 }
 
+salaAttrezzi = {
+        "nomeSala": "Mostra degli attrezzi",
+        "temaSala": "La mostra degli attrezzi tradizionali per l'estrazione e la lavorazione della pietra di Antonio Girardi",
+        "descrizione": "Nel complesso museale Cava Bomba è presente un cortiletto interno, provvisto di una tettoia, originariamente utilizzata come stalla. Nei primi anni di funzionamento del museo questo spazio è servito per le pause delle scolaresche in visita d'istruzione. \
+                        Negli anni 1996-97 si è presentata una buona opportunità: il custode del museo, Antonio Girardi, propose di mettere a disposizione per l'esposizione una singolare raccolta di attrezzi tradizionali per il lavoro nelle cave e per la lavorazione della trachite. \
+                        Gli spazi espositivi del complesso erano già tutti occupati ma, data la natura degli oggetti da esporre, si ritenne che la tettoia del cortiletto poteva benissimo quindi ospitare la raccolta senza vetrine, ma con semplici strutture di distanziamento. \
+                        La mostra consta di tre sezioni. Da un lato sono esposti gli attrezzi che servivano per sbancare gli strati di calcare e per frantumare le pietre che dovevano alimentare la fornace, quindi relativi alla cava di calcare; nel lato opposto ci sono invece gli attrezzi per il taglio e la lavorazione della trachite. \
+                        Nel mezzo, forgia, grande trapano manuale a colonna, incudine ecc., richiamano l'officina del fabbro dove si forgiavano e si riparavano tutti gli attrezzi. \
+                        Tutti i reperti esposti sono stati raccolti con passione e competenza da Antonio Girardi, custode del museo Cava Bomba fino al 2006, anno della sua scomparsa. \
+                        Nella sezione dedicata alla lavorazione in cava della trachite e riolite compaiono, tra i vari oggetti, le “penole” e i “levarini” per staccare la roccia in blocchi dalla parete di cava, i “punciotti” per rompere la pietra a terra a colpi di mazza, le punte della bocciarda per la rifinitura. \
+                        Il filo d'acciaio elicoidale, azionato da un motore, costituisce la tecnica di taglio che sostituisce i cunei o “punciotti” e che viene rappresentata da un modellino in scala realizzato da Antonio Girardi.",
+        "imgMainSala": "static/imgs/salaAttrezzi/mainSalaAttrezzi.jpg"
+}
+
+salaArcheologia = {
+        "nomeSala": "Archeologia industriale",
+        "temaSala": "Il percorso dedicato all'archeologia industriale",
+        "descrizione": "Le cave di calcare dei Colli Euganei hanno sempre rifornito i forni di cottura dei centri maggiori di Padova, Este e Monselice; nella seconda metà dell'Ottocento, grazie alle migliorate condizioni della rete viaria, è risultato conveniente l'impianto di fornaci da calce anche in numerose località periferiche presso le cave presenti nei Colli (Marendole, Lozzo, Baone, Bastia di Rovolon, ecc.). \
+                        L'impianto di Cava Bomba rappresenta tra questi il complesso più imponente nella produzione di calce dei Colli Euganei. Il primo impianto è rappresentato da un tipico forno a tino, risalente all'ultimo decennio del XIX secolo (la data precisa non è nota); successivamente la produzione viene incrementata con la costruzione di altri due forni funzionanti analogamente al primo, ma inglobati in una massiccia costruzione a base quadrata. \
+                        Man mano che aumenta l'attività della fornace, culminata nei primi decenni del '900, si rende necessaria la costruzione di nuove strutture edilizie (magazzini, locali di servizio per i fornaciai, stalla per gli animali da soma, ecc.). \
+                        Dopo il restauro, realizzato dal Consorzio sotto la guida della Soprintendenza ai Beni Ambientali e Architettonici, il complesso è ritornato alla sua fisionomia originale, salvo l'abbattimento di alcune parti eccessivamente degradate. \
+                        Il percorso ottimale ideato per la visita, guidata o autonoma, è supportato da una serie di pannelli didascalici che illustrano la struttura e il funzionamento dei forni e la sequenza del processo produttivo.",
+        "imgMainSala": "static/imgs/salaArcheologia/mainSalaArcheologia.png"
+}
+
 """
 HOME CAROUSEL IMGS
 Per aggiungere immagini al carosello della pagina principale in modo dinamico: 
@@ -89,6 +114,9 @@ homeCarouselImgs = getImgs("static/HomeCarouselImgs/", "img")
 sala1Imgs = getImgs("static/imgs/salaA/","")
 sala2Imgs = getImgs("static/imgs/salaB/","")
 sala3Imgs = getImgs("static/imgs/salaC/","")
+salaAttrezziImgs = getImgs("static/imgs/salaAttrezzi/","")
+salaArcheologiaImgs = getImgs("static/imgs/salaArcheologia/","")
+didatticaImgs = getImgs("static/imgs/didattica/","")
 
 slogans = getSlogans("static/slogans/slogans.txt")
 
@@ -114,21 +142,18 @@ def museo():
 def storia():
         return render_template("storia.html")
 
-@app.route("/attrezzi")
-def attrezzi():
-        return render_template("attrezzi.html")
-
-@app.route("/archeologia")
-def archeologia():
-        return render_template("archeologia.html")
-
 @app.route("/ambiente")
 def ambiente():
         return render_template("ambiente.html")
 
 @app.route("/didattica")
 def didattica():
-        return render_template("didattica.html")
+        return render_template("didattica.html", images=didatticaImgs)
+
+@app.errorhandler(404)
+def paginaNonTrovata(e):
+        print("Errore? ", e)
+        return render_template("404.html"), 404
 
 
 # Una stanza per sala o una sala per tre stanze?
@@ -138,7 +163,7 @@ def didattica():
 def stanza(indice):
         try:
                 indice = int(indice)
-                if(indice >=1 and indice<=nsale):
+                if(indice >=1 and indice<=NSALE):
                         sala = "sala.html"
                         
                         datiSala = {}
@@ -153,6 +178,12 @@ def stanza(indice):
                         elif(indice==3):
                                 datiSala = sala3
                                 immagini = sala3Imgs
+                        elif(indice==4):
+                                datiSala = salaAttrezzi
+                                immagini = salaAttrezziImgs
+                        elif(indice==5):
+                                datiSala = salaArcheologia
+                                immagini = salaArcheologiaImgs
 
                         return render_template(sala, images=immagini, **datiSala)
                 else:
